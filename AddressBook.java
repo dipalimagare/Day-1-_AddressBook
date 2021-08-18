cbbe8286cd5550f3c3b64dc102a483d0c59763b0
dipalimagare <kalpeshmahajan90@gmail.com>
2021-08-18 16:46:11 +0530

    Uc4

--- AddressBook.java
+++ AddressBook.java
@@ -2,45 +2,50 @@ package com.bridgelabz;
 
 import java.util.Scanner;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
-	Person p = new Person();
-	static Person[] group = new Person[5];
+
+	static ArrayList<Person> contact = new ArrayList<Person>();
+	static Scanner scan = new Scanner(System.in);
+	static int totalObj = 0;
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++) {
-			System.out.println("Enter Contact of Person" + (i + 1));
-			group[i] = new Person();
-			getInfo(group[i]);
+		int ans = 1;
+		while (ans == 1) {
+			System.out.println("Enter Contact");
+			Person p = new Person();
+			getInfo(p);
+			contact.add(p);
+			System.out.println("Want to Add more? Enter 1 for Yes, 2 for No");
+			ans = scan.nextInt();
+			totalObj += 1;
 		}
+
 		System.out.println("Want to Edit? Enter 1 for Yes, 2 for No");
 		Scanner sc = new Scanner(System.in);
 		if (sc.nextInt() == 1) {
 			searchPerson();
 		}
-			System.out.println("Want to Delete? Enter 1 for Yes, 2 for No");
-			if (sc.nextInt() == 1) {
-				deletePerson();
-			}
+
+		System.out.println("Want to Delete? Enter 1 for Yes, 2 for No");
+		if (sc.nextInt() == 1) {
+			deletePerson();
 		}
-	
+	}
 
 	public static void deletePerson() {
 		System.out.println("\nEnter Fisrt Name: ");
 		Scanner sc = new Scanner(System.in);
 		String firstName = sc.next();
 		int flag = 0;
-		for (int i = 0; i < 5; i++) {
-			System.out.println(group[i].fName);
-			if (firstName.equals(group[i].fName)) {
-				group[i].address = null;
-				group[i].city = null;
-				group[i].email = null;
-				group[i].fName = null;
-				group[i].lName = null;
-				group[i].pNumber = 0;
-				group[i].state = null;
-				group[i].zip = 0;
+		for (int i = 0; i < totalObj; i++) {
+			if (firstName.equals(contact.get(i).fName)) {
+				contact.remove(i);
+				totalObj -= 1;
 				System.out.println("Deleted..!!");
+				flag = 1;
 			}
 		}
 		if (flag == 0) {
@@ -53,11 +58,11 @@ public class AddressBook {
 		Scanner sc = new Scanner(System.in);
 		String firstName = sc.next();
 		int flag = 0;
-		for (int i = 0; i < 5; i++) {
-			System.out.println(group[i].fName);
-			if (firstName.equals(group[i].fName)) {
+		for (int i = 0; i < totalObj; i++) {
+			System.out.println(contact.get(i).fName);
+			if (firstName.equals(contact.get(i).fName)) {
 				System.out.println("\nEdit---->");
-				getInfo(group[i]);
+				getInfo(contact.get(i));
 				flag = 1;
 				break;
 			}
@@ -88,4 +93,80 @@ public class AddressBook {
 		System.out.println("Enter Email: ");
 		p1.setEmail(sc.next());
 	}
+}
+
+class Person {
+	String fName;
+	String lName;
+	String address;
+	String city;
+	String state;
+	int zip;
+	long pNumber;
+	String email;
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public long getpNumber() {
+		return pNumber;
+	}
+
+	public void setpNumber(long pNumber) {
+		this.pNumber = pNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
